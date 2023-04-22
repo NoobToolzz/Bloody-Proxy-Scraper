aea10863de202692ccb9a1c07e3af25dc4e7b8ca
NoNoobz <131222159+NoobToolzz@users.noreply.github.com>
2023-04-22 18:26:06 +1000

    Moved updater to data\plugins\common.py

--- Bloody Proxy Scraper V2.py
+++ Bloody Proxy Scraper V2.py
@@ -5,18 +5,13 @@ from console.utils import set_title
 from colorama import Fore
 
 from data.sources import http_urls, socks4_urls, socks5_urls, all_urls
-from data.antiskid import urFucked
+from data.plugins.common import Updater
+from data.plugins.antiskid import urFucked
 from win10toast import ToastNotifier
 toast = ToastNotifier()
 
-# Updater
-def Updater():
-    AutoUpdate.set_url("https://github.com/NoobToolzz/Bloody-Proxy-Scraper/blob/main/data/version.txt")
-    AutoUpdate.set_download_link("https://raw.githubusercontent.com/NoobToolzz/Bloody-Proxy-Scraper/main/Bloody%20Proxy%20Scraper%20V2.py")
-    AutoUpdate.set_current_version("v2.0.3")
-
-    if not AutoUpdate.is_up_to_date():
-        AutoUpdate.download("Bloody Proxy Scraper V2.py")
+# Check for updates
+Updater()
 
 # Anti-Skid
 __author__ = 'NoNoobz'
