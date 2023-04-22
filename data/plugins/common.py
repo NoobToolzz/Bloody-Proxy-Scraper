f0687ea7796af3a487147a288ec7c898f4e867ae
NoNoobz <131222159+NoobToolzz@users.noreply.github.com>
2023-04-22 18:21:23 +1000

    Add files via upload

---
+++ data/plugins/common.py
@@ -0,0 +1,10 @@
+import AutoUpdate
+
+# Updater
+def Updater():
+    AutoUpdate.set_url("https://github.com/NoobToolzz/Bloody-Proxy-Scraper/blob/main/data/version.txt")
+    AutoUpdate.set_download_link("https://raw.githubusercontent.com/NoobToolzz/Bloody-Proxy-Scraper/main/Bloody%20Proxy%20Scraper%20V2.py")
+    AutoUpdate.set_current_version("v2.0.3")
+
+    if not AutoUpdate.is_up_to_date():
+        AutoUpdate.download("Bloody Proxy Scraper V2.py")
