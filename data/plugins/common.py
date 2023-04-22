7746c76dd2a0037ef1f7cea645a27877cf694c68
NoNoobz <131222159+NoobToolzz@users.noreply.github.com>
2023-04-22 18:26:27 +1000

    Update common.py

--- data/plugins/common.py
+++ data/plugins/common.py
@@ -4,7 +4,7 @@ import AutoUpdate
 def Updater():
     AutoUpdate.set_url("https://github.com/NoobToolzz/Bloody-Proxy-Scraper/blob/main/data/version.txt")
     AutoUpdate.set_download_link("https://raw.githubusercontent.com/NoobToolzz/Bloody-Proxy-Scraper/main/Bloody%20Proxy%20Scraper%20V2.py")
-    AutoUpdate.set_current_version("v2.0.3")
+    AutoUpdate.set_current_version("v2.0.4")
 
     if not AutoUpdate.is_up_to_date():
         AutoUpdate.download("Bloody Proxy Scraper V2.py")
