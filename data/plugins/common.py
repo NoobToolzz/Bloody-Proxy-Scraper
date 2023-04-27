3bb91ceac7dc89ca804601874dbfbaee15a40b69
NoNoobz <131222159+NoobToolzz@users.noreply.github.com>
2023-04-27 18:42:14 +1000

    Added updaters to 2 more files
    Added updaters to the following
    data/plugins/common.py
    data/plugins/antiskid.py

--- data/plugins/common.py
+++ data/plugins/common.py
@@ -1,10 +1,34 @@
+import os
 import AutoUpdate
 
-# Updater
-def Updater():
+# Updaters
+def MainUpdater():
     AutoUpdate.set_url("https://github.com/NoobToolzz/Bloody-Proxy-Scraper/blob/main/data/version.txt")
     AutoUpdate.set_download_link("https://raw.githubusercontent.com/NoobToolzz/Bloody-Proxy-Scraper/main/Bloody%20Proxy%20Scraper%20V2.py")
-    AutoUpdate.set_current_version("v2.0.4")
+    AutoUpdate.set_current_version("v2.0.5")
 
     if not AutoUpdate.is_up_to_date():
         AutoUpdate.download("Bloody Proxy Scraper V2.py")
+
+def CommonUpdater():
+    AutoUpdate.set_url("https://github.com/NoobToolzz/Bloody-Proxy-Scraper/blob/main/data/version.txt")
+    AutoUpdate.set_download_link("https://raw.githubusercontent.com/NoobToolzz/Bloody-Proxy-Scraper/main/data/plugins/common.py")
+    AutoUpdate.set_current_version("v2.0.5")
+
+    if not AutoUpdate.is_up_to_date():
+        AutoUpdate.download("common.py")
+
+    os.remove("data/plugins/common.py")
+    os.rename("common.py", "data/plugins/common.py")
+
+def SkidUpdater():
+    AutoUpdate.set_url("https://github.com/NoobToolzz/Bloody-Proxy-Scraper/blob/main/data/version.txt")
+    AutoUpdate.set_download_link("https://raw.githubusercontent.com/NoobToolzz/Bloody-Proxy-Scraper/main/data/plugins/antiskid.py")
+    AutoUpdate.set_current_version("v2.0.5")
+
+    if not AutoUpdate.is_up_to_date():
+        AutoUpdate.download("antiskid.py")
+
+    # directory = os.getcwd()
+    os.remove("data/plugins/antiskid.py")
+    os.rename("antiskid.py", "data/plugins/antiskid.py")
